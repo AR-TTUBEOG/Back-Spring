056f6982608595cefb7d74df7b8bdf8d900c6199
choeun7 <short0720@gmail.com>
2024-01-16 15:57:14 +0900

    [Feat] 혜택 생성 api
    혜택 생성 api를 작성했습니다.
    Store은 아직 완성이 안된 상태라 주석처리했습니다.

--- src/main/java/com/ttubeog/domain/benefit/application/BenefitService.java
+++ src/main/java/com/ttubeog/domain/benefit/application/BenefitService.java
@@ -1,11 +1,66 @@
 package com.ttubeog.domain.benefit.application;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.ttubeog.domain.benefit.domain.Benefit;
+import com.ttubeog.domain.benefit.domain.repository.BenefitRepository;
+import com.ttubeog.domain.benefit.dto.request.CreateBenefitReq;
+import com.ttubeog.domain.benefit.dto.response.CreateBenefitRes;
+import com.ttubeog.domain.member.domain.Member;
+import com.ttubeog.domain.member.domain.repository.MemberRepository;
+import com.ttubeog.domain.store.domain.Store;
+import com.ttubeog.domain.store.domain.repository.StoreRepository;
+import com.ttubeog.global.DefaultAssert;
+import com.ttubeog.global.config.security.token.UserPrincipal;
+import com.ttubeog.global.payload.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 @Transactional(readOnly = true)
 public class BenefitService {
+
+    private final MemberRepository memberRepository;
+    private final BenefitRepository benefitRepository;
+    private final StoreRepository storeRepository;
+
+    // 혜택 생성
+    @Transactional
+    public ResponseEntity<?> createBenefit(UserPrincipal userPrincipal, CreateBenefitReq createBenefitReq) throws JsonProcessingException {
+
+        Optional<Member> userOptional = memberRepository.findById(userPrincipal.getId());
+        DefaultAssert.isOptionalPresent(userOptional);
+        Member member = userOptional.get();
+
+//        Optional<Store> storeOptional = storeRepository.findById(createBenefitReq.getStoreId());
+//        Store store;
+//        DefaultAssert.isOptionalPresent(storeOptional);
+//        store = storeOptional.get();
+
+        Benefit benefit = Benefit.builder()
+                .content(createBenefitReq.getContent())
+                .type(createBenefitReq.getType())
+//                .store(store)
+                .build();
+
+        benefitRepository.save(benefit);
+
+        CreateBenefitRes createBenefitRes = CreateBenefitRes.builder()
+                .benefitId(benefit.getId())
+//                .storeId(benefit.getStore().getId())
+                .content(benefit.getContent())
+                .type(benefit.getType())
+                .build();
+
+        ApiResponse apiResponse = ApiResponse.builder()
+                .check(true)
+                .information(createBenefitRes)
+                .build();
+
+        return ResponseEntity.ok(apiResponse);
+    }
 }
--- src/main/java/com/ttubeog/domain/benefit/presentation/BenefitController.java
+++ src/main/java/com/ttubeog/domain/benefit/presentation/BenefitController.java
@@ -1,7 +1,44 @@
 package com.ttubeog.domain.benefit.presentation;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.ttubeog.domain.benefit.application.BenefitService;
+import com.ttubeog.domain.benefit.dto.request.CreateBenefitReq;
+import com.ttubeog.domain.benefit.dto.response.CreateBenefitRes;
+import com.ttubeog.global.config.security.token.CurrentUser;
+import com.ttubeog.global.config.security.token.UserPrincipal;
+import com.ttubeog.global.payload.ErrorResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Benefit", description = "Benefit API")
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/benefit")
 public class BenefitController {
+
+    private final BenefitService benefitService;
+
+    //혜택 생성
+    @Operation(summary = "혜택 생성", description = "매장의 혜택을 생성합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "혜택 생성 성공", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = CreateBenefitRes.class) ) } ),
+            @ApiResponse(responseCode = "400", description = "혜택 생성 실패", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class) ) } ),
+    })
+    @PostMapping
+    public ResponseEntity<?> createBenefit(
+            @Parameter(description = "Accesstoken을 입력해주세요.", required = true)
+            @CurrentUser UserPrincipal userPrincipal,
+            @Valid @RequestBody CreateBenefitReq createBenefitReq
+    ) throws JsonProcessingException {
+        return benefitService.createBenefit(userPrincipal, createBenefitReq);
+    }
 }
