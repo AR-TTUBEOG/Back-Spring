0d96b4df8af0c553a857f58147c8e2fe850f0998
choeun7 <short0720@gmail.com>
2024-01-16 15:52:03 +0900

    [Feat] Benefit 도메인 초기 설정
    Benefit과 BenefitType을 설정했습니다.

--- src/main/java/com/ttubeog/domain/benefit/domain/Benefit.java
+++ src/main/java/com/ttubeog/domain/benefit/domain/Benefit.java
@@ -1,15 +1,36 @@
 package com.ttubeog.domain.benefit.domain;
 
 import com.ttubeog.domain.common.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.ttubeog.domain.store.domain.Store;
+import jakarta.persistence.*;
+import lombok.*;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 @Entity
+@Table(name = "benefit")
 public class Benefit extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "type")
+    @Enumerated(EnumType.STRING)
+    private BenefitType type;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @Builder
+    public Benefit(Long id, String content, BenefitType type, Store store) {
+        this.id = id;
+        this.content = content;
+        this.type = type;
+        this.store = store;
+    }
 }
