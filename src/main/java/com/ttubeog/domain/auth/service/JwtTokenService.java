8db6adb995125a0bf68a9a2818b15dd56b3c3e67
arinming <jyandysys1@naver.com>
2024-01-16 17:53:37 +0900

    [FEAT] JWT를 통한 accessToken, refreshToken 생성 로직 추가 (#1)

---
+++ src/main/java/com/ttubeog/domain/auth/service/JwtTokenService.java
@@ -0,0 +1,86 @@
+package com.ttubeog.domain.auth.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.io.Encoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.Random;
+
+@Service
+public class JwtTokenService implements InitializingBean {
+
+    private final String secretKey;
+    private final long accessTokenExpirationInSeconds;
+    private final long refreshTokenExpirationInSeconds;
+    private static Key key;
+
+
+    public JwtTokenService(
+            @Value("${jwt.secret-key}") String secretKey,
+            @Value("${jwt.access-expired-time}") long accessTokenExpirationInSeconds,
+            @Value("${jwt.refresh-expired-time}") long refreshTokenExpirationInSeconds
+    ) {
+        this.secretKey = secretKey;
+        this.accessTokenExpirationInSeconds = accessTokenExpirationInSeconds;
+        this.refreshTokenExpirationInSeconds = refreshTokenExpirationInSeconds;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        key = getKeyFromBase64EncodedKey(encodeBase64SecretKey(secretKey));
+    }
+
+    // JWT 토큰 생성
+    public String createToken(String payload, long expireLength) {
+        // 토큰에 포함될 정보
+        Claims claims = Jwts.claims().setSubject(payload);
+        // 현재 날짜와 시간
+        Date now = new Date();
+        // 유효기간
+        Date validity = new Date(now.getTime() + expireLength);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(key, SignatureAlgorithm.HS256) // 토큰 서명
+                .compact();
+    }
+
+    // 액세스 토큰 생성
+    public String createAccessToken(String payload) {
+        return createToken(payload, accessTokenExpirationInSeconds);
+    }
+
+    // 리프레시 토큰 생성
+    public String createRefreshToken() {
+        byte[] array = new byte[7];
+        new Random().nextBytes(array);
+        String newPayload = new String(array, StandardCharsets.UTF_8);
+
+        return createToken(newPayload, refreshTokenExpirationInSeconds);
+    }
+
+
+
+    private String encodeBase64SecretKey(String secretKey) {
+        return Encoders.BASE64.encode(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private Key getKeyFromBase64EncodedKey(String encodedSecretKey) {
+        byte[] keyBytes = Decoders.BASE64.decode(encodedSecretKey);
+
+        Key key = Keys.hmacShaKeyFor(keyBytes);
+
+        return key;
+    }
+}
