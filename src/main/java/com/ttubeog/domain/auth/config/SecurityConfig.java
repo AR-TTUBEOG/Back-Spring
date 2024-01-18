ed76baf999f7a1f895486ad0d3dae561d592effb
arinming <jyandysys1@naver.com>
2024-01-18 14:24:53 +0900

    [FEAT] SecurityConfig 추가 (#1)

---
+++ src/main/java/com/ttubeog/domain/auth/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.ttubeog.domain.auth.config;
+
+import com.ttubeog.domain.auth.application.JwtTokenService;
+import com.ttubeog.domain.member.application.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig {
+    private final JwtTokenService jwtTokenService;
+    private final MemberService memberService;
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            final AuthenticationConfiguration authenticationConfiguration
+            ) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain configure(final HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.cors(withDefaults())
+                .csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/auth/login/**", "/token/refresh").permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer.disable())    // 기본 로그인 폼 미사용
+                .httpBasic(httpSecurityHttpBasicConfigurer -> httpSecurityHttpBasicConfigurer.disable())    // 기본 http 미사용
+                .build();
+    }
+}
