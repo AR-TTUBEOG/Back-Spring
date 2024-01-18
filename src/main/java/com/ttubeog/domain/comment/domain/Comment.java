3f8cc3c08a8ff32ec8080ffec35d9f13dd545a56
Jeongmin39 <wjdals20309@naver.com>
2024-01-18 16:01:17 +0900

    [Feat] Comment 도메인 작성

--- src/main/java/com/ttubeog/domain/comment/domain/Comment.java
+++ src/main/java/com/ttubeog/domain/comment/domain/Comment.java
@@ -1,15 +1,38 @@
 package com.ttubeog.domain.comment.domain;
 
 import com.ttubeog.domain.common.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.ttubeog.domain.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "comment")
 public class Comment extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "latitude")
+    private Float latitude;
+
+    @Column(name = "longitude")
+    private Float longitude;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Builder
+    public Comment(Long id, String content, Float latitude, Float longitude) {
+        this.id = id;
+        this.content = content;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 }
