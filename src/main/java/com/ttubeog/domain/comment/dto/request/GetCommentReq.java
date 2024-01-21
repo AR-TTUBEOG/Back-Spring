5e605bdb4635fc8a933f65e24614e2a1a7d0937a
Jeongmin39 <wjdals20309@naver.com>
2024-01-21 13:48:45 +0900

    [Feat] Comment 조회 구현

---
+++ src/main/java/com/ttubeog/domain/comment/dto/request/GetCommentReq.java
@@ -0,0 +1,15 @@
+package com.ttubeog.domain.comment.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Data
+public class GetCommentReq {
+
+    @Schema(description = "사용자 위치 위도값")
+    private Float latitude;
+
+    @Schema(description = "사용자 위치 경도값")
+    private Float longitude;
+
+}
