54bd3765643907cd278b30905a2b0d63ec8c2065
arinming <jyandysys1@naver.com>
2024-01-23 18:55:40 +0900

    [Feat] Apple Login Public Key 생셩 구현 (#16)

--- src/main/java/com/ttubeog/domain/auth/exception/ErrorCode.java
+++ src/main/java/com/ttubeog/domain/auth/exception/ErrorCode.java
@@ -7,7 +7,11 @@ public enum ErrorCode {
     INVALID_ACCESS_TOKEN("유효하지 않은 액세스 토큰입니다.", HttpStatus.UNAUTHORIZED),
     INVALID_REFRESH_TOKEN("유효하지 않은 리프레시 토큰입니다.", HttpStatus.UNAUTHORIZED),
     BAD_REQUEST("잘못된 요청입니다.", HttpStatus.BAD_REQUEST),
-    NOT_EXIST_USER("존재하지 않는 유저입니다.", HttpStatus.UNAUTHORIZED);
+    NOT_EXIST_USER("존재하지 않는 유저입니다.", HttpStatus.UNAUTHORIZED),
+    INVALID_APPLE_OAUTH_TOKEN("Apple OAuth Identity Token 값이 올바르지 않습니다.", HttpStatus.UNAUTHORIZED),
+    PUBLIC_KEY_GENERATION_ERROR("Apple OAuth 로그인 중 public key 생성에 문제가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+    APPLE_OAUTH_TOKEN_EXPIRED("Apple OAuth 로그인 중 Identity Token 유효기간이 만료됐습니다.", HttpStatus.UNAUTHORIZED);
+
 
     private final String message;
     private final HttpStatus httpStatus;
