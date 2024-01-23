4f0c5d8d7e71a50ce83092ae1de5c9cd2cc23998
Jeongmin39 <wjdals20309@naver.com>
2024-01-23 16:40:53 +0900

    [Feat] Store 도메인 작성

--- src/main/java/com/ttubeog/domain/store/domain/Store.java
+++ src/main/java/com/ttubeog/domain/store/domain/Store.java
@@ -1,15 +1,55 @@
 package com.ttubeog.domain.store.domain;
 
+import com.ttubeog.domain.area.domain.DongArea;
 import com.ttubeog.domain.common.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.ttubeog.domain.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
 
 @Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "store")
 public class Store extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "info")
+    private String info;
+
+    @Column(name = "detailAddress")
+    private String detailAddress;
+
+    @Column(name = "latitude")
+    private Float latitude;
+
+    @Column(name = "longitude")
+    private Float longitude;
+
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "stars")
+    private Float stars;
+
+    @Column(name = "type")
+    @Enumerated(EnumType.STRING)
+    private StoreType type;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "dong_area_id")
+    private DongArea dongArea;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 }
