2ad5702b64c8961e3bf3866bf2c15d5f63f937d6
Jeongmin39 <wjdals20309@naver.com>
2024-01-25 23:11:34 +0900

    [Feat] Store 등록 구현

--- src/main/java/com/ttubeog/domain/area/domain/DongArea.java
+++ src/main/java/com/ttubeog/domain/area/domain/DongArea.java
@@ -1,15 +1,22 @@
 package com.ttubeog.domain.area.domain;
 
 import com.ttubeog.domain.common.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "dong_area")
 public class DongArea extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    public DongArea(Long id) {
+        this.id = id;
+    }
 }
