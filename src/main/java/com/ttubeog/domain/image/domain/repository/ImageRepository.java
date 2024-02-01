b72fea114fa4df38d3a6003e00eb5ce5da891bb4
sanggae4133 <sanggae4133@naver.com>
2024-02-01 20:44:24 +0900

    [Feat] compare to merge

--- src/main/java/com/ttubeog/domain/image/domain/repository/ImageRepository.java
+++ src/main/java/com/ttubeog/domain/image/domain/repository/ImageRepository.java
@@ -3,5 +3,13 @@ package com.ttubeog.domain.image.domain.repository;
 import com.ttubeog.domain.image.domain.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ImageRepository extends JpaRepository<Image, Long> {
+
+    @Override
+    List<Image> findAll();
+
+    <Image> findBySpotId(Long id);
 }
--- src/main/java/com/ttubeog/domain/spot/application/SpotService.java
+++ src/main/java/com/ttubeog/domain/spot/application/SpotService.java
@@ -1,15 +1,26 @@
 package com.ttubeog.domain.spot.application;
 
+import com.ttubeog.domain.area.domain.DongArea;
+import com.ttubeog.domain.area.domain.repository.DongAreaRepository;
+import com.ttubeog.domain.image.domain.Image;
+import com.ttubeog.domain.image.domain.repository.ImageRepository;
+import com.ttubeog.domain.member.domain.Member;
 import com.ttubeog.domain.member.domain.repository.MemberRepository;
 import com.ttubeog.domain.member.exception.InvalidMemberException;
+import com.ttubeog.domain.spot.domain.Spot;
 import com.ttubeog.domain.spot.domain.repository.SpotRepository;
 import com.ttubeog.domain.spot.dto.request.CreateSpotRequestDto;
 import com.ttubeog.domain.spot.dto.request.UpdateSpotRequestDto;
+import com.ttubeog.domain.spot.exception.AlreadyExistsSpotException;
 import com.ttubeog.global.config.security.token.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.awt.font.ImageGraphicAttribute;
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -18,10 +29,38 @@ public class SpotService {
 
     private final SpotRepository spotRepository;
     private final MemberRepository memberRepository;
+    private final DongAreaRepository dongAreaRepository;
+    private final ImageRepository imageRepository;
 
     public ResponseEntity<?> createSpot(UserPrincipal userPrincipal, CreateSpotRequestDto createSpotRequestDto) {
 
         memberRepository.findById(userPrincipal.getId()).orElseThrow(InvalidMemberException::new);
+
+        if (spotRepository.findByName(createSpotRequestDto.getName()).isPresent()) {
+            throw new AlreadyExistsSpotException();
+        }
+
+        Optional<Member> memberOptional = memberRepository.findById(createSpotRequestDto.getMemberId());
+        Optional<DongArea> dongAreaOptional = dongAreaRepository.findById(createSpotRequestDto.getDongAreaId());
+        List<Image> imageOptional = imageRepository.findBySpotId(createSpotRequestDto.getId());
+        if (memberOptional.isPresent() && dongAreaOptional.isPresent() && imageOptional.isPresent()) {
+            Member member = memberOptional.get();
+            DongArea dongArea = dongAreaOptional.get();
+            Image image = imageOptional.get();
+
+            Spot spot = Spot.builder()
+                    .member(member)
+                    .dongArea(dongArea)
+                    .detailAddress(createSpotRequestDto.getDetailAddress())
+                    .name(createSpotRequestDto.getName())
+                    .info(createSpotRequestDto.getInfo())
+                    .latitude(createSpotRequestDto.getLatitude())
+                    .longitude(createSpotRequestDto.getLongitude())
+                    .images(image)
+                    .build();
+        }
+
+
 
         return null;
     }
