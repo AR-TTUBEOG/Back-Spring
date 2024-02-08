e5f998fdf5259c80facd9169f4b9a74876fef9a7
Jeongmin39 <wjdals20309@naver.com>
2024-02-09 06:13:46 +0900

    [Feat] DongArea 관현 에러 추가

--- src/main/java/com/ttubeog/domain/area/domain/DongArea.java
+++ src/main/java/com/ttubeog/domain/area/domain/DongArea.java
@@ -2,21 +2,16 @@ package com.ttubeog.domain.area.domain;
 
 import com.ttubeog.domain.common.BaseEntity;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "dong_area")
+@NoArgsConstructor
 public class DongArea extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public DongArea(Long id) {
-        this.id = id;
-    }
 }
