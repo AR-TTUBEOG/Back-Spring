7834d73a85b451916b0e472cdb4de73dff0b569f
Dustbox <sanggae4133@naver.com>
2024-02-15 17:48:28 +0900

    [Feat] implement Road Entity

--- src/main/java/com/ttubeog/domain/road/domain/Road.java
+++ src/main/java/com/ttubeog/domain/road/domain/Road.java
@@ -1,10 +1,10 @@
 package com.ttubeog.domain.road.domain;
 
 import com.ttubeog.domain.common.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.ttubeog.domain.member.domain.Member;
+import com.ttubeog.domain.spot.domain.Spot;
+import com.ttubeog.domain.store.domain.Store;
+import jakarta.persistence.*;
 
 @Entity
 public class Road extends BaseEntity {
@@ -12,4 +12,25 @@ public class Road extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "spot_id")
+    private Spot spot;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "road_file", nullable = false)
+    private String roadFile;
+
+    @Column(name = "time", nullable = false)
+    private String time;
 }
