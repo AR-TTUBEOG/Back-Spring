5df95a722113b707210457aac2fa30d4ec79bcec
Dustbox <sanggae4133@naver.com>
2024-02-17 20:19:49 +0900

    [Refac] Spot 엔티티 dongArea String으로 수정

--- src/main/java/com/ttubeog/domain/spot/dto/request/UpdateSpotRequestDto.java
+++ src/main/java/com/ttubeog/domain/spot/dto/request/UpdateSpotRequestDto.java
@@ -16,7 +16,7 @@ public class UpdateSpotRequestDto {
     private String name;
 
     @Schema(description = "지역 코드 ID", defaultValue = "1")
-    private Long dongAreaId;
+    private String dongAreaId;
 
     @Schema(description = "상세 주소", defaultValue = "1층")
     private String detailAddress;
