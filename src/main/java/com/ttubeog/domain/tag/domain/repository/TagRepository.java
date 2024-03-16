0258f8bfea4113b04c464eee7c6d2b3a23ab0bf4
Jeongmin39 <wjdals20309@naver.com>
2024-03-16 18:52:08 +0900

    [Remove] Tag 관련 파일 제거

--- src/main/java/com/ttubeog/domain/tag/domain/repository/TagRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ttubeog.domain.tag.domain.repository;
-
-import com.ttubeog.domain.tag.domain.Tag;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TagRepository extends JpaRepository<Tag, Long> {
-}
